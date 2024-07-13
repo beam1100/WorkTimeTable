3a90ae20abd4e47aa7d76cd042702869802e9b2d
beam1100 <beam1100@naver.com>
2024-07-13 09:51:18 +0900

    123

--- app/src/main/java/com/worktimetable/TableFragment.kt
+++ app/src/main/java/com/worktimetable/TableFragment.kt
@@ -1,7 +1,7 @@
 package com.worktimetable
-
 import android.app.Dialog
 import android.content.Context
+//import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
@@ -11,14 +11,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.LinearLayout
 import android.widget.Spinner
 import android.widget.TableLayout
 import android.widget.TableRow
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
+import androidx.core.view.children
 import com.worktimetable.databinding.FragmentTableBinding
-import kotlin.math.log
+
 
 
 class TableFragment : Fragment() {
@@ -33,6 +35,7 @@ class TableFragment : Fragment() {
     private val vBinding get() = _vBinding!!
     private lateinit var mainActivity:MainActivity
     private var logMapList = arrayListOf<HashMap<String,Any>>()
+    private var selectedDate:String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -136,19 +139,26 @@ class TableFragment : Fragment() {
             val tableLayout = TableLayout(requireContext())
             for(typeMap in typeList){
                 val tableRow = TableRow(requireContext())
+                val type = typeMap["type"] as String
+                val isConcurrent = typeMap["isConcurrent"] as Boolean
                 for(shiftMap in shiftList){
+                    val shift = shiftMap["shift"] as String
                     AppCompatButton(requireContext()).apply {
                         tableRow.addView(this)
-                        mainActivity.getMapByCondition(
-                            logMapList,
-                            hashMapOf("type" to typeMap["type"] as String, "shift" to shiftMap["shift"] as String)
-                        )?.set("btn", this)
+                        val logMap = mainActivity.getMapByCondition( logMapList, hashMapOf("type" to type, "shift" to shift))?.apply { set("btn", this) }
                         this.setOnClickListener {
-                            Toast.makeText(requireContext(), "클릭이벤트", Toast.LENGTH_SHORT).show()
+                            val alreadySelected = logMap?.get("member") as List<String>
+                            val otherTimeSelected = if(isConcurrent){
+                                listOf()
+                            }else{
+                                getSameTimeMember(shift).filterNot { it in alreadySelected}
+                            }
+                            mkCheckMemberDialog( alreadySelected, otherTimeSelected ){ checkedMember->
+                                text = checkedMember.joinToString("\n")
+                                logMap["member"] = checkedMember
+                            }
                         }
                         setBtnStyle(this, androidx.appcompat.R.color.material_grey_300, 200, 200)
-
-
                     }
                 }
                 tableLayout.addView(tableRow)
@@ -190,6 +200,17 @@ class TableFragment : Fragment() {
         }
     } // mkTable End
 
+    private fun getSameTimeMember(shift: String): List<String> {
+        val resultSet = mutableSetOf<String>()
+        val sameTimeMap = logMapList.filter {it["shift"] == shift}
+        sameTimeMap.forEach {map->
+            (map["member"] as ArrayList<String>).forEach {name->
+                resultSet.add(name)
+            }
+        }
+        return resultSet.toList()
+    }
+
 
     private fun setBtnStyle(btn: AppCompatButton, backGroundColor:Int, width:Int, height:Int){
         btn.setBackgroundColor(ContextCompat.getColor(requireContext(), backGroundColor))
@@ -198,6 +219,67 @@ class TableFragment : Fragment() {
         params.setMargins(3, 3, 3, 3)
         btn.layoutParams = params
         btn.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+    }
+
+    private fun mkCheckMemberDialog(
+        alreadySelected:List<String>,
+        otherTimeSelected:List<String>,
+        callbackChecked: (ArrayList<String>) -> Unit
+    ) {
+        Dialog(requireContext()).apply {
+            setContentView(R.layout.dialog_check_member)
+            mainActivity.setDialogSize(this, vBinding.tableFragmentLayout, 0.9f, null)
+            show()
+
+            val mainMemberHolderLayout = findViewById<LinearLayout>(R.id.mainMemberLayout)
+            val subMemberHolderLayout = findViewById<LinearLayout>(R.id.subMemberLayout)
+            val inflater = LayoutInflater.from(requireContext())
+
+            mainActivity.helper.select("MemberTable", toSortColumn = "sortIndex").map { it["memberName"] }.onEach {memberName->
+                memberName as String
+                val myCheckbox = inflater.inflate(R.layout.custom_checkbox, null) as CheckBox
+                myCheckbox.text = memberName
+                if(memberName in otherTimeSelected){
+                    myCheckbox.isEnabled = false
+                }
+                if(memberName in alreadySelected){
+                    myCheckbox.isChecked = true
+                }
+                mainMemberHolderLayout.addView(myCheckbox)
+            }
+
+            listOf("지원1", "지원2", "지원3").onEach {memberName->
+                val myCheckbox = (inflater.inflate(R.layout.custom_checkbox, null) as CheckBox)
+                memberName as String
+                myCheckbox.text = memberName
+                if(memberName in otherTimeSelected){
+                    myCheckbox.isEnabled = false
+                }
+                if(memberName in alreadySelected){
+                    myCheckbox.isChecked = true
+                }
+                subMemberHolderLayout.addView(myCheckbox)
+            }
+            findViewById<Button>(R.id.checkMemberBtn).setOnClickListener {
+                val checkedMember = arrayListOf<String>()
+                mainMemberHolderLayout.children.forEach {view->
+                    if(view is CheckBox){
+                        if(view.isChecked){
+                            checkedMember.add(view.text.toString())
+                        }
+                    }
+                }
+                subMemberHolderLayout.children.forEach {view->
+                    if(view is CheckBox){
+                        if(view.isChecked){
+                            checkedMember.add(view.text.toString())
+                        }
+                    }
+                }
+                callbackChecked(checkedMember)
+                dismiss()
+            }
+        }
     }
 
 
